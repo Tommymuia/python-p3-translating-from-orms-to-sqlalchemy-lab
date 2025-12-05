f7ce0aa79d0c01b01be9d6d1fc551d6a6edd3c74
Tommymuia <muiathomas.mt@gmail.com>
2025-12-05 17:19:28 +0300

    added

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,54 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(Base, engine):
+    """
+    Creates all tables for the Base metadata.
+    """
+    Base.metadata.create_all(engine)
+
 
 def save(session, dog):
-    pass
+    """
+    Saves a Dog instance to the DB.
+    """
+    session.add(dog)
+    session.commit()
+
 
 def get_all(session):
-    pass
+    """
+    Returns all Dog instances.
+    """
+    return session.query(Dog).all()
+
 
 def find_by_name(session, name):
-    pass
+    """
+    Finds a Dog by name.
+    """
+    return session.query(Dog).filter_by(name=name).first()
+
 
 def find_by_id(session, id):
-    pass
+    """
+    Finds a Dog by ID.
+    """
+    return session.query(Dog).filter_by(id=id).first()
+
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    """
+    Finds a Dog by name and breed.
+    """
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
-def update_breed(session, dog, breed):
-    pass
+
+def update_breed(session, dog, new_breed):
+    """
+    Updates a Dog's breed and commits changes.
+    """
+    dog.breed = new_breed
+    session.commit()
--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python3
-
-from sqlalchemy import (Column, String, Integer)
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 class Dog(Base):
-    __tablename__ = 'dogs'
+    __tablename__ = "dogs"
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    breed = Column(String())
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    breed = Column(String)
+
+    def __repr__(self):
+        return f"<Dog id={self.id} name={self.name} breed={self.breed}>"
